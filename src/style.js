79e87e0bf5384e5dfd10d9536a1aa900375e8dd4
Elena <tandaraelena@yahoo.com>
2019-09-20 08:57:51 +0100

    useState and useEffect

---
+++ src/style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const StyledSection = styled.div`
+  padding: 5px;
+
+  > div {
+    padding: 1px;
+  }
+`
+export const StyledHeader = styled.div`
+  color: red;
+`
